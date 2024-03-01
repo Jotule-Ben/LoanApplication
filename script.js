d9c0b20bad8da552a94ed6e312fc67ad83046f3c
Matthew Benedicta <matthewbenedicta2007@gmail.com>
2024-03-01 02:38:03 +0100

    Design a loan app and implent all functionalities

---
+++ script.js
@@ -0,0 +1,161 @@
+let points = 0;
+const accountBalance = 210000;
+
+
+function getRequired() {
+  let inputField = document.getElementById("userLoanAmount");
+  let input = inputField.value;
+  if (input === "") {
+    inputField.style.borderColor = "red";
+    let error = document.getElementById("errorMessageLoanAmount");
+    error.style.color = "red";
+    error.textContent = "Enter Your Loan Amount";
+    error.style.fontSize = "12px";
+  } else if (input <= accountBalance * 0.45) {
+    points += 10;
+  } else {
+    points -= 10;
+  }
+}
+
+document
+  .getElementById("creditHistory")
+  .addEventListener("change", function () {
+    let creditScore = document.getElementById("creditHistory");
+
+    if (creditScore.value !== "") {
+      creditScore.classList.remove("error");
+    }
+  });
+
+function creditScoreChecker() {
+  let creditScore = document.getElementById("creditHistory");
+  if (creditScore.value === "") {
+    creditScore.classList.add("error");
+    errorMessageCredit.textContent = "Select an option";
+    // errorMessageCredit.append(errorMessageCredit);
+  } else if (creditScore >= 1) {
+    points = +10;
+    console.log(points);
+  }
+}
+
+document.getElementById("depositDate").addEventListener("change", function () {
+  let lastDepositValue = document.getElementById("depositDate");
+
+  if (lastDepositValue.value !== "") {
+    lastDepositValue.classList.remove("error");
+  }
+});
+
+function lastDepositDate() {
+ let lastDepositValue = document.getElementById("depositDate");
+ if (lastDepositValue.value === "") {
+   lastDepositValue.classList.add("error");
+  //  errorMessageDate.textContent = "Select a Date";
+ } else {
+   let userDepositDate = new Date(lastDepositValue);
+   let today = new Date();
+
+   let differenceInMs = today - userDepositDate;
+
+   let differenceInDays = Math.floor(differenceInMs / (1000 * 60 * 60 * 24));
+
+   if (differenceInDays <= 30) {
+     points += 5;
+   }
+ }
+}
+
+document.getElementById("lastLoan").addEventListener("change", function () {
+  let lastLoanValue = document.getElementById("lastLoan");
+
+  if (lastLoanValue.value !== "") {
+    lastLoanValue.classList.remove("error");
+  }
+});
+
+function lastLoanDate() {
+  let lastLoanValue = document.getElementById("lastLoan");
+  if (lastLoanValue.value === "") {
+    lastLoanValue.classList.add("error");
+    // lastLoanValue.textContent = "Select a Date";
+  }else{
+  let lastLoanValue = document.getElementById("lastLoan").value;
+  let userLoanDate = new Date(lastLoanValue);
+  let today = new Date();
+
+  let differenceInMs = today - userLoanDate;
+
+  let differenceInDays = Math.floor(differenceInMs / (1000 * 60 * 60 * 24));
+
+  if (differenceInDays >= 180) {
+    points += 10;
+  }
+  console.log(points);
+}
+}
+
+
+document
+  .getElementById("loanRepayment")
+  .addEventListener("change", function () {
+    let loanRepaymentValue = document.getElementById("loanRepayment");
+
+    if (loanRepaymentValue.value !== "") {
+      loanRepaymentValue.classList.remove("error");
+    }
+  });
+
+function loanRepaymentDate() {
+  let loanRepaymentValue = document.getElementById("loanRepayment");
+  if (loanRepaymentValue.value === "") {
+    loanRepaymentValue.classList.add("error");
+  }else{
+  let loanRepaymentValue = document.getElementById("loanRepayment").value;
+  let userLoanTenure = new Date(loanRepaymentValue);
+  let today = new Date();
+
+  let differenceInMs = userLoanTenure - today;
+
+  let differenceInDays = Math.floor(differenceInMs / (1000 * 60 * 60 * 24));
+
+  if (differenceInDays <= 180) {
+    points += 10;
+  }
+}
+}
+
+document.getElementById("accounttype").addEventListener("change", function () {
+  let accType = document.getElementById("accounttype");
+
+  if (accType.value !== "") {
+    accType.classList.remove("error");
+  }
+});
+
+function accountTypeChecker() {
+  let accType = document.getElementById("accounttype");
+  if (accType.value === "") {
+    accType.classList.add("error");
+    errorMessageAcc.textContent = "Select an option";
+  } else if (accType >= 1) {
+    points += 10;
+  } else {
+    points += 5;
+  }
+}
+
+function getLoan() {
+  getRequired();
+  creditScoreChecker();
+  lastDepositDate();
+  lastLoanDate();
+  loanRepaymentDate();
+  accountTypeChecker();
+  if (points >= 30) {
+    window.location.href = "approved.html";
+  } else if (points <= 30) {
+    window.location.href = "Unapproved.html";
+  }
+}
